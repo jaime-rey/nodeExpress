26f372d6cf04bdf3108e870c3066a10904f915fa
Jaime <jaimereycasado@gmail.com>
2017-01-29 02:46:19 +0100

    third commit. adding git

--- project/meadowlark.js
+++ project/meadowlark.js
@@ -1,23 +1,45 @@
 /**
  * Created by Jaime on 29/01/2017.
  */
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+
+let handlebars = require('express-handlebars').create({ defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
 app.set('port', process.env.PORT || 3000);
-// custom 404 page
-app.use(function(req, res){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+
+app.get('/', (req, res) => {
+    var randomFortune =
+        fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('home', { fortune: randomFortune} );
 });
-// custom 500 page
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
+
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate.' );
 });
+
+let fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
