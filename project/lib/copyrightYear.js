3464f8442b6c33f0a977439816baee02ae6bb8ee
Jaime <jaimereycasado@gmail.com>
2017-02-05 02:54:05 +0100

    sixth commit form handling pg90

---
+++ project/lib/copyrightYear.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jaime on 05/02/2017.
+ */
+exports.getCopyrightYear = function() {
+    return 2017;
+};
--- project/meadowlark.js
+++ project/meadowlark.js
@@ -3,13 +3,29 @@
  */
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var copyrightYear = require('./lib/copyrightYear.js');
 var app = express();
 
-var handlebars = require('express-handlebars').create({ defaultLayout:'main'});
+var bodyParser  = require('body-parser');
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if (!this._sections){
+                this._sections = {};
+            }
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded( { extended: true }));
 
+app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 
 app.use(function(req, res, next)  {
@@ -19,7 +35,8 @@ app.use(function(req, res, next)  {
 });
 
 app.get('/', function(req, res) {
-    res.render('home', { fortune: fortune.getFortune() } );
+    res.render('home', { fortune: fortune.getFortune(),
+        copyrightYear: copyrightYear.getCopyrightYear() } );
 });
 app.get('/about', function(req, res) {
     res.render('about', {
@@ -45,6 +62,26 @@ app.get('/greeting', function(req, res){
         style: req.query.style
     });
 });
+
+app.get('/newsletter', function(req, res){
+// we will learn about CSRF later...for now, we just
+// provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.get('/thank-you', function (req, res) {
+    res.render('thank-you');
+})
+
+
+app.post('/process', function(req, res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
+});
+
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
     res.status(404).render('404');
@@ -67,4 +104,38 @@ var tours = [
 
 app.get('/api/tours', function(req, res){
     res.json(tours);
+});
+
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+app.use(function(req, res, next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
+    next();
 });
